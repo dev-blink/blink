61f3453ffa0a0cdf4bb5a2be335a213af602453e
aaix <>
2020-05-08 04:17:54 +0100

    add snipe command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,13 +1,38 @@
 import discord
 from discord.ext import commands
-import random
+from collections import deque
 import blink
 import pyfiglet
+import datetime
+import random
 
 
 class Fun(commands.Cog,name="Fun"):
     def __init__(self,bot):
         self.bot=bot
+        self.snipes = {}
+
+    @commands.Cog.listener("on_message_delete")
+    async def append_snipes(self,message):
+        if len(message.content) > 1024 or message.author.bot or not message.content:
+            return
+        snipes = self.snipes.get(message.guild.id)
+        if not snipes:
+            snipes = deque([],5)
+        snipes.appendleft((message.content,str(message.author),datetime.datetime.utcnow()))
+        self.snipes[message.guild.id] = snipes
+
+    @commands.command(name="snipe")
+    @commands.guild_only()
+    @commands.bot_has_guild_permissions(send_messages=True,embed_links=True)
+    async def snipe(self,ctx):
+        snipes = self.snipes.get(ctx.guild.id)
+        if not snipes:
+            return await ctx.send("No snipes found")
+        embed= discord.Embed(title="Deleted messages",colour=self.bot.colour)
+        for snipe in snipes:
+            embed.add_field(name=f"{snipe[1]} deleted {blink.prettydelta((datetime.datetime.utcnow() - snipe[2]).total_seconds())} ago",value=snipe[0],inline=False)
+        return await ctx.send(embed=embed)
 
     @commands.command(name="random")
     @commands.guild_only()
