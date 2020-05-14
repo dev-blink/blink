fa0e58a87ecbf26af8ecfd0cc47b13e495aa9086
aaix <>
2020-05-14 02:47:10 +0100

    swappeth nodeth

--- cogs/music.py
+++ cogs/music.py
@@ -285,7 +285,7 @@ class Music(commands.Cog):
         if self.bot.user.name == "blink beta":
             nodes={'MAIN': {'host': '192.168.1.154','port': 2333,'rest_uri': 'http://192.168.1.154:2333','password': 'password','identifier': 'MAIN','region': 'us_east'}}
         else:
-            nodes={'MAIN': {'host': 'localhost','port': 2333,'rest_uri': 'http://10.128.0.9:2333','password': 'password','identifier': 'MAIN','region': 'us_east'}}
+            nodes={'MAIN': {'host': '10.128.0.9','port': 2333,'rest_uri': 'http://10.128.0.9:2333','password': 'password','identifier': 'MAIN','region': 'us_east'}}
 
         for n in nodes.values():
             node=await self.bot.wavelink.initiate_node(**n)
