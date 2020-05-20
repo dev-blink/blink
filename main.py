be4857beeebcee84489e52db539cf9c3e8cdf06d
aaix <>
2020-05-20 15:01:59 +0100

    change stuff

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ def get_prefix(bot, message):
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
 
-print("Initializing AutoShardedBot and global vars.")
 loading_extensions=["cogs.help","cogs.member","cogs.dev","cogs.info","cogs.error","cogs.mod","cogs.server","cogs.fun","cogs.roles","cogs.advancedinfo","cogs.stats","cogs.media","cogs.DBL","cogs.logging","cogs.sql","cogs.nsfw","cogs.music"]
 loading_extensions.append("jishaku")
 SHARD_COUNT = 2
@@ -44,8 +43,7 @@ os.environ["JISHAKU_HIDE"] = "True"
 os.environ["JISHAKU_RETAIN"] = "True"
 
 bot=commands.AutoShardedBot(command_prefix=get_prefix, description="Blink!",activity=discord.Game(name="Bot is starting, please wait."),status=discord.Status.dnd,help_command=None,shard_count=SHARD_COUNT,shard_ids=SHARD_IDS,case_insensitive=True)
-bot.load_extension("cogs.PRELOAD")
-print("LOADED PRELOAD ERROR HANDLER")
+bot.load_extension("cogs.pre-error")
 bot.startingcogs=loading_extensions
 bot.colour=0xf5a6b9
 loadexceptions=""
@@ -53,16 +51,13 @@ bot.INITIALIZED=False
 
 
 def load_extensions():
-    print("Initializing Extensions")
     for extension in loading_extensions:
         try:
             bot.load_extension(extension)
-            print("Loaded: " + extension)
         except Exception as e:
             print("unable to load: " + extension + " Exception was raised: " + str(e))
             global loadexceptions
             loadexceptions=loadexceptions + f"Unable to load: {extension} Exception was raised: {e}\n"
-    print("Finished loading cogs\n")
 
 
 @bot.event
@@ -85,28 +80,16 @@ async def on_shard_ready(id):
 
 
 async def __init():
-    print("\nINIT DATABASE")
     cn={"user":"blink","password":"local","database":"main","host":"localhost"}
     bot.DB=await asyncpg.create_pool(**cn)
-    print("DATABASE INITIALIZED")
-    print("\nINIT HTTP SESSION")
     bot.session = aiohttp.ClientSession()
-    print("HTTP INITIALIZED\n")
-    print(f'\nLogged in as: {bot.user.name} - {bot.user.id}\nDiscord.py Version: {discord.__version__}')
     bot.statsserver=bot.get_guild(blink.Config.statsserver())
-    if not bot.user.name == "blink beta":
-        game=discord.Streaming(name='; (b;)', url='https://www.twitch.tv/#')
-    else:
-        game=discord.Game(name="beta bot")
-    await bot.change_presence(status=discord.Status.online,activity=game)
+    await bot.change_presence(status=discord.Status.online,activity=discord.Streaming(name='; (b;)', url='https://www.twitch.tv/#'))
     bot.boottime=STARTUPTIME
-    bot.unload_extension("cogs.PRELOAD")
+    bot.unload_extension("cogs.pre-error")
     load_extensions()
-    print(f'Successfully logged in and booted.')
-    print("Server Count: " + str(len(bot.guilds)) + "\nShard Count: " + str(bot.shard_count))
     boottime=datetime.datetime.utcnow() - STARTUPTIME
     members=len(list(bot.get_all_members()))
-    print(f"STARTUP COMPLETED IN : {boottime} ({round(members / boottime.total_seconds(),2)} members / second)")
     startupid=blink.Config.startup(bot.user.name)
     if not loadexceptions == "":
         x=f"\n***ERRORS OCCURED ON STARTUP:***\n{loadexceptions}"
@@ -116,6 +99,7 @@ async def __init():
     if startupid:
         await bot.get_channel(startupid).send(boot)
     bot.bootlog=boot
+    print("READY")
 
 
 bot.run(open("TOKEN","r").read(), bot=True, reconnect=True)
