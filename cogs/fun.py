bd307dfcdafae4779627b16c30b7daef38e3e6ee
aaix <>
2020-06-03 23:02:15 +0100

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -17,7 +17,11 @@ class Fun(commands.Cog,name="Fun"):
     async def append_snipes(self,message):
         if len(message.content) > 1024 or message.author.bot or not message.content:
             return
-        snipes = self.snipes.get(message.guild.id).get(message.channel.id)
+        g = self.snipes.get(message.guild.id)
+        if g is None:
+            g = {}
+            self.snipes[message.guild.id] = {}
+        snipes = g.get(message.channel.id)
         if not snipes:
             snipes = deque([],5)
         snipes.appendleft((message.content,str(message.author),datetime.datetime.utcnow()))
@@ -41,9 +45,13 @@ class Fun(commands.Cog,name="Fun"):
 
     @commands.Cog.listener("on_message_edit")
     async def append_edit_snipes(self,before,after):
-        if (len(before.content) + len(after.content)) > 1024 or before.author.bot or not before.content:
+        if (len(before.content) + len(after.content)) > 1024 or before.author.bot or not before.content or before.content == after.content:
             return
-        snipes = self.esnipes.get(before.guild.id).get(before.channel.id)
+        g = self.esnipes.get(before.guild.id)
+        if g is None:
+            g = {}
+            self.esnipes[before.guild.id] = {}
+        snipes = g.get(before.channel.id)
         if not snipes:
             snipes = deque([],5)
         snipes.appendleft((f"{before.content} **🠢** {after.content}",str(before.author),datetime.datetime.utcnow()))
