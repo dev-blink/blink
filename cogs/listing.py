5fcd9b93e3dfae43b3d1e1fe5997dc9aa58c08bf
aaix <git@aaix.pro>
2020-07-20 17:30:42 +0100

    close aiohttp

--- cogs/listing.py
+++ cogs/listing.py
@@ -25,16 +25,22 @@ class ListingHandler(commands.Cog):
             r = await cs.post("https://top.gg/api/bots/692738917236998154/stats",data={"server_count":guilds},headers={"Authorization":self.tokens["dbl"]})
             if not r.status == 200:
                 await self.bot.warn(f"Error in DBL post, response {r.status} `{await r.json()}`",False)
+            cs.close()
+            cs._connector.close()
 
         async with aiohttp.ClientSession() as cs:
             r = await cs.post("https://api.botsdatabase.com/v1/bots/692738917236998154",json={"servers":guilds},headers={"Authorization":self.tokens["bdb"],"Content-Type":"application/json"})
             if not r.status == 200:
                 await self.bot.warn(f"Error in BDB post, response {r.status} `{await r.json()}`",False)
+            cs.close()
+            cs._connector.close()
 
         async with aiohttp.ClientSession() as cs:
             r = await cs.post("https://api.discordextremelist.xyz/v2/bot/692738917236998154/stats",json={"guildCount":guilds},headers={"Authorization":self.tokens["del"],"Content-Type":"application/json"})
             if not r.status == 200:
                 await self.bot.warn(f"Error in DEL post, response {r.status} `{await r.json()}`",False)
+            cs.close()
+            cs._connector.close()
 
 
 def setup(bot):
