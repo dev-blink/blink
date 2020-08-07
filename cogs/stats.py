bdc46804a079fc59493705f4898b5cf99021d55e
aaix <git@aaix.pro>
2020-08-07 11:16:33 +0100

    clean up code

--- cogs/stats.py
+++ cogs/stats.py
@@ -33,19 +33,19 @@ class Stats(commands.Cog,name="Stats"):
 
     @commands.Cog.listener("on_guild_join")
     async def guild_join(self,guild):
-        embed=discord.Embed(title=f"Bot added to guild {guild.id}",colour=0x00ff3c,description=f"Total guilds: {self.bot.cluster.guilds+1}")
+        embed=discord.Embed(title=f"Bot added to guild {guild.id} on cluster {self.bot.cluster.name}",colour=0x00ff3c,description=f"Total guilds: {self.bot.cluster.guilds+1}")
         embed.add_field(name="Guild name:",value=f"{guild.name}")
         embed.add_field(name="Guild members:",value=f"{guild.member_count}")
-        embed.add_field(name="Guild owner:",value=f"{guild.owner.name}#{guild.owner.discriminator}\n{guild.owner.mention}")
+        embed.add_field(name="Guild owner:",value=f"{guild.owner}\n{guild.owner.mention}")
         embed.set_thumbnail(url=guild.icon_url_as(static_format="png"))
         await self.bot.cluster.log_guilds(embed=embed)
 
     @commands.Cog.listener("on_guild_remove")
     async def guild_remove(self,guild):
-        embed=discord.Embed(title=f"Bot removed from guild {guild.id}",colour=0xff0003,description=f"Total guilds: {self.bot.cluster.guilds-1}")
+        embed=discord.Embed(title=f"Bot removed from guild {guild.id} on cluster {self.bot.cluster.name}",colour=0xff0003,description=f"Total guilds: {self.bot.cluster.guilds-1}")
         embed.add_field(name="Guild name:",value=f"{guild.name}")
         embed.add_field(name="Guild members:",value=f"{guild.member_count}")
-        embed.add_field(name="Guild owner:",value=f"{guild.owner.name}#{guild.owner.discriminator}\n{guild.owner.mention}")
+        embed.add_field(name="Guild owner:",value=f"{guild.owner}\n{guild.owner.mention}")
         embed.set_thumbnail(url=guild.icon_url_as(static_format="png"))
         await self.bot.cluster.log_guilds(embed=embed)
 
