3f6931761b7d61312cb6f08ecc2057ff4a79bc35
aaix <55102797+aaix@users.noreply.github.com>
2021-03-10 19:58:32 +0000

    add sql tables :)#

---
+++ tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE blacklist (scope TEXT PRIMARY KEY, snowflakes bigint ARRAY)
+CREATE TABLE userlog (id bigint PRIMARY KEY, name TEXT ARRAY, avatar TEXT ARRAY)
+CREATE TABLE social (id bigint PRIMARY KEY, hugs TEXT ARRAY, kisses TEXT ARRAY, relation bigint, ship TEXT, blocked bigint ARRAY)
+CREATE TABLE ships (id TEXT PRIMARY KEY, captain bigint, partner bigint,name TEXT,customtext TEXT,colour bigint,icon TEXT,timestamp bigint)
+CREATE TABLE globalmsg (id bigint PRIMARY KEY, messages bigint)
+CREATE TABLE guilds (id bigint PRIMARY KEY, data TEXT)
