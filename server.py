8d3d7619bd34a65bef9d9ad77634c73c87408567
aaix <>
2022-04-18 17:31:46 +0100

    rnadom comments

--- server.py
+++ server.py
@@ -18,20 +18,20 @@ import datetime
 import aiohttp
 from blink import CacheDict
 
-tokens = config.gatewayauth
+tokens = config.gatewayauth # list of tokens that clients are allowed to authenticate with
 loop = asyncio.get_event_loop()
 
 
 async def _panic(message: str, cluster: str):
+    """Send a push notification on an unclean disconnect"""
     data = {
         "app_key": config.panic_app_key,
         "app_secret": config.panic_app_secret,
         "access_token": config.panic_access_token,
         "content": message,
         "target_type": "user",
-
-
     }
+
     async with aiohttp.ClientSession() as cs:
         async with cs.post("https://api.pushed.co/1/push", data=data, headers={"content-type": "application/x-www-form-urlencoded"}) as response:
             print(
@@ -39,12 +39,14 @@ async def _panic(message: str, cluster: str):
 
 
 class Message:
+    """Custom class to abstract websocket messages"""
     def __init__(self, op: int, data: dict):
         self.op = op
         self.data = data
 
 
 class Intent:
+    """Class to abstract sending data to clusters"""
     def __init__(self, intent: str, data: dict):
         self.op = 3
         self.intent = intent.upper()
@@ -55,12 +57,13 @@ class Intent:
 
 
 class ServerProtocol(websocket.WebSocketServerProtocol):
+    """Main class that handles a single cluster connection"""
     def __init__(self):
         super().__init__()
         self.sequence = 0
         self.authenticated = False
         self.open = False
-        self.ops = [0, 1, 2, 3, 4, 5, 6, 7]
+        self.ops = [0, 1, 2, 3, 4, 5, 6, 7] # List of valid opcodes
         self.beating = False
         self.handlers = {
             0: self.invalid_opcode,
@@ -74,6 +77,7 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
         }
 
     async def broadcast(self, payload):
+        """Send a message to all other clusters connected"""
         if payload.get("intent") is None:
             return await self.close(code=4001, error="No intent provided")
         if payload.get("content") is None:
@@ -81,7 +85,7 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
         await self.factory.broadcast(client=self.sessionID, event=Intent(intent=payload["intent"], data=payload["content"]))
 
     async def identify(self, payload):
-
+        """Handle clusters initial connection"""
         if payload.get("authorization") is None:
             return await self.close(code=4004, error="No client authorization")
         if payload["authorization"] not in tokens:
@@ -95,6 +99,7 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
         self.factory.register(self)
 
     async def dedupe(self, payload):
+        """Handle a deduplication request"""
         if payload.get("scope") is None:
             return await self.close(code=4001, error="No scope provided")
         if payload.get("content") is None:
@@ -104,21 +109,25 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
         dupe = self.factory.dedupe(payload["scope"], payload["content"])
         return await self.send(6, {"duplicate": dupe, "req": payload["req"]})
 
-    async def event(self, payload):
+    async def event(self, payload): # Skeleton code, event opcode is not used
+        """Handles a cluster sending an event"""
         if payload.get("intent") is None:
             return await self.close(code=4001, error="No intent for event payload")
         print(f"EVENT intent='{payload['intent']}'")
 
     async def panicked(self, payload):
+        """A cluster has panicked, save its log and close it"""
         with open(f"Cluster {self.identifier} crash.log", "w+") as f:
             f.write(f"{payload.get('error')}\n{payload.get('traceback')}")
         await self.close(4999, "Client exception thrown")
         await _panic(f"Cluster {self.identifier} Has crashed {payload.get('error')}", self.identifier)
 
     async def ack(self, op):
+        """After every packet is recieved, an 'ack' is sent"""
         await self.send(4, {"recieved": op})
 
     async def decode(self, payload: bytes):
+        """Deserialise a payload into a Message object"""
         try:
             payload = json.loads(payload.decode(encoding="utf-8"))
         except json.decoder.JSONDecodeError:
@@ -136,9 +145,14 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
         return Message(op=payload["op"], data=payload["data"])
 
     async def invalid_opcode(self, payload):
+        """
+        Fallback function for unknown opcodes
+        Called by the handler not the deserialiser
+        """
         await self.close(code=4003, error="Payload opcode was not acceptable")
 
     async def send(self, op, payload):
+        """Send data to the clusters websocket and serialise it"""
         self.sequence += 1
         data = {
             "op": op,
@@ -149,6 +163,7 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
         self.sendMessage(raw)
 
     async def close(self, code: int, error: str):
+        """Close the websocket cleanly"""
         self.authenticated = False
         self.open = False
         self.sendClose(code=code, reason=error)
@@ -157,9 +172,10 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
         print(f"\n[GATEWAY]Client connection from [{request.peer}]")
 
     async def onOpen(self):
+        """Send the hello payload after a cluster connects"""
         cluster = await self.factory.getCluster(self)
         if not cluster:
-            return
+            return # Abandon hello because cluster pool is full
         self.identifier = cluster
         self.open = True
         self.sessionID = str(uuid.uuid4())
@@ -176,19 +192,22 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
         await loop.create_task(self.heartbeat())
 
     async def onMessage(self, payload, isBinary):
+        """Handle a raw message from a cluster"""
         payload = await self.decode(payload)
         if payload is None:
             return
         if not self.authenticated and payload.op != 1:
+            # Clusters should not send any payload before they have identified
             return await self.close(code=4005, error="Not authenticated")
-        await self.handlers[payload.op](payload.data)
+        await self.handlers[payload.op](payload.data) # Call handler for opcode sent
         if self.open:
             try:
-                await self.ack(payload.op)
+                await self.ack(payload.op) # Ack payload
             except Disconnected:
                 pass
 
     async def onClose(self, isClean, code, reason):
+        """Panic if a cluster didnt close properly"""
         self.open = False
         self.factory.unregister(self)
         print(
@@ -197,14 +216,20 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
             await _panic(f"Cluster {self.identifier} Has closed uncleanly with reason {code} - {reason}", self.identifier)
 
     async def dispatch(self, client: str, event: Intent):
+        """
+        Used for broadcast, sends a message to the cluster
+        Will not send if the current cluster broadcasted it
+        """
         if client == self.sessionID:
             return
         await self.send(op=event.op, payload=event.data)
 
     async def registerHeartbeat(self, payload):
+        """Acknowledge a heartbeat"""
         self.beating = True
 
     async def heartbeat(self):
+        """Continually check if a heartbeat has been recieved"""
         try:
             while self.open:
                 if not await self.heartbeatCheck():
@@ -214,6 +239,7 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
                 await self.close(code=4006, error="No heartbeat recieved")
 
     async def heartbeatCheck(self):
+        """Check if a heartbeat has been reciebed in the last interval"""
         async with timeout(self.heartbeatInterval):
             while self.open:
                 await asyncio.sleep(1)
@@ -225,11 +251,12 @@ class ServerProtocol(websocket.WebSocketServerProtocol):
 
 
 class Factory(websocket.WebSocketServerFactory):
+    """Factory class to spawn websockets for each cluster"""
     def __init__(self):
         super().__init__()
         self.clients = []
-        self.registered_dupes = CacheDict(50_000)
-        self.protocol = ServerProtocol
+        self.registered_dupes = CacheDict(50_000) # Dictionary limited in size for memory efficiency
+        self.protocol = ServerProtocol # The websocket class to handle each connection
 
     def register(self, client):
         if client not in self.clients:
@@ -244,10 +271,16 @@ class Factory(websocket.WebSocketServerFactory):
             self.clients.remove(client)
 
     async def broadcast(self, client: str, event: Intent):
+        """Send a message to all connected clusters"""
         for c in self.clients:
             await c.dispatch(client, event)
 
     def dedupe(self, scope, hash):
+        """
+        Clusters can request that a key be unique across all clusters
+        this function will store all keys and return true or false if
+        the key is unique or not, keys may not be unique across scopes
+        """
         if not self.registered_dupes.get(scope):
             self.registered_dupes[scope] = {}
         if self.registered_dupes[scope].get(hash) is None:
@@ -256,12 +289,16 @@ class Factory(websocket.WebSocketServerFactory):
         return True
 
     async def getCluster(self, client):
+        """Get an identifier for a cluster"""
         if len(self.clients) == config.clusters:
             await client.close(4007, "Too many clusters")
             return
-        return [i for i in alphabet[:config.clusters] if i not in [client.identifier for client in self.clients]][0]
+        # This list comprehension iterates over the alphabet until a letter is found that is not a registered cluster
+        # It will return the first letter found
+        return next(i for i in alphabet[:config.clusters] if i not in [client.identifier for client in self.clients])
 
 
+# Set up asyncio event loop and run the factory server
 loop = asyncio.get_event_loop()
 coro = loop.create_server(Factory(), '0.0.0.0', 9000)
 server = loop.run_until_complete(coro)
